3ec6d11a1b2ba6269c9c5aa68d14b190cb058834
luismarquez96c <luismarquez96c@gmail.com>
2023-11-04 15:16:50 -0700

    Logro: creación de proyecto inicial

---
+++ src/main/java/net/luismarquez/projects/SpringBootIoC/SpringBootIoCApplication.java
@@ -0,0 +1,13 @@
+package net.luismarquez.projects.SpringBootIoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/luismarquez/projects/SpringBootIoC/SpringBootIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package net.luismarquez.projects.SpringBootIoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
